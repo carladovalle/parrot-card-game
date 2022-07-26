c61414dde4d10ea483ce9b1f40b4eb422e4b0eb2
Carla <carla.valle@engenharia.ufjf.br>
2022-07-26 13:34:36 -0300

    feat: distribution of cards

---
+++ scripts.js
@@ -0,0 +1,39 @@
+let numberCards;
+const imgs = ["images/bobrossparrot.gif","images/bobrossparrot.gif",
+            "images/explodyparrot.gif","images/explodyparrot.gif",
+            "images/fiestaparrot.gif","images/fiestaparrot.gif",
+            "images/metalparrot.gif","images/metalparrot.gif",
+            "images/revertitparrot.gif","images/revertitparrot.gif",
+            "images/tripletsparrot.gif","images/tripletsparrot.gif",
+            "images/unicornparrot.gif","images/unicornparrot.gif"]
+
+function game() {
+
+    let numberCards = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um valor entre 4 e 14."));
+    while (numberCards < 4 || numberCards > 14 || (numberCards % 2) === 1) {
+        numberCards = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um valor entre 4 e 14."));
+    }
+
+    function comparador() {
+        return Math.random() - 0.5;
+    }
+      
+    let imgsShuffled = imgs.slice(0,(numberCards));
+    imgsShuffled.sort(comparador);
+
+    for (let i = 0; i < numberCards; i++) {
+        const list = document.querySelector(".container");
+        list.innerHTML += `
+                <div class="card">
+                    <div class="front-face face">
+                        <img class="imageParrot" src="images/front.png">
+                    </div>
+                    <div class="back-face face">
+                        <img class="imageAnimations" src=${imgsShuffled[i]} />
+                    </div>
+                </div>
+        `
+    }
+}
+
+game(); 
